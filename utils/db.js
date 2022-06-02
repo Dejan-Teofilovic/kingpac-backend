4758b206aec7b449ac8829a05497fe5dade0291c
DejanTeofilovic <dejanteofilovic2@gmail.com>
2022-06-03 08:12:48 +0900

    ..

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const db = require("serverless-mysql")({
   config: {
     database: process.env.DB_NAME,
     user: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
   },
